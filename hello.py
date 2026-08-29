d3a746ee5768c92844e799265836df38f27fa728
Ousmane diallo <kouste9@gmail.com>
2026-08-29 22:58:42 +0900

    new file

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = input("what's your name? ")
+year = input("how many years you are? ")
+print(f"hello {name}, you are {year} old")
+
+freinds = ["ousmane", "paul", "salia"]
+for freinds in freinds:
+    print(freinds)
