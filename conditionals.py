5baa68860fc267a518bf2095dee216ef203663c8
Ousmane diallo <kouste9@gmail.com>
2026-09-02 18:03:22 +0000

    add

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,5 @@
-"""name = input("what's your name: ")
-if name == "Kouste":
+name = input("what's your name: ")
+if name == "ouste":
     print("you are so a sweet !")
 else:
-    print(f"you are so stupid {name}")"""
-    
+    print(f"you are so stupid {name}")
