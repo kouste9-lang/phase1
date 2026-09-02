71c0f2ef3021ef623e0a7a64ab05f4aae7f7cdcc
Ousmane diallo <kouste9@gmail.com>
2026-09-02 20:08:47 +0000

    dictionarie

--- list.py
+++ list.py
@@ -1,4 +1,14 @@
-friends = ["ousmane", "diallo", "paul"]
+"""friends = ["ousmane", "diallo", "paul"]
 friends.append("oumar")
 friends[1] = "kouste"
-print(friends[0:])
+print(friends[0:])"""
+
+# dictionaries
+monthConversion = {
+    "jan": "january",
+    "feb": "february",
+    "mar": "march",
+    "apr": "april",
+}
+
+print(monthConversion.get("jan"))
