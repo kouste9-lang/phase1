5ed2ae5529a69e150b8191b8b705623dff2a3692
Ousmane diallo <kouste9@gmail.com>
2026-09-02 22:28:48 +0000

    first game

--- guess.py
+++ guess.py
@@ -1,6 +1,18 @@
-secret_word="ouste"
-guess=""
-while guess != secret_word:
-    guess=input("enter the secret word: ")
+secret_word = "ouste"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-print("you guessed the secret word")
+while guess != secret_word and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = input("enter the secret word: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+        break
+
+if out_of_guesses:
+    print("you are out of guesses, you lose!")
+else:
+    print("you win!")
