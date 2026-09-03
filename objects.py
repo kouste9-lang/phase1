8146632999f67236e30db1d9a93227df08594fbe
Ousmane diallo <kouste9@gmail.com>
2026-09-03 12:45:35 +0000

    new file

---
+++ objects.py
@@ -0,0 +1,8 @@
+from student import Student
+
+student1 = Student("paul", "Computer Science", False, 3.5)
+
+print(f"Name: {student1.name}")
+print(f"Major: {student1.major}")
+print(f"Is on probation: {student1.is_on_probation}")
+print(f"GPA: {student1.gpa}")
---
+++ student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, major, is_on_probation, gpa):
+        self.name = name
+        self.major = major
+        self.is_on_probation = is_on_probation
+        self.gpa = gpa
